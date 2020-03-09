8ffd0c765ea47994d0c8bb5846f0e778731edaf3
Donsan <mengtao_zh@163.com>
2020-03-09 18:47:13 +0800

    first construction

---
+++ qf-shop-service/qf-shop-search-service/src/main/java/com/qf/QfShopSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QfShopSearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QfShopSearchServiceApplication.class, args);
+    }
+
+}
