569950cc5881892998a8e619dee92519ed497aba
高富国 <gfg1637473849@163.com>
2020-03-18 22:13:25 +0800

    first construction

--- qf-shop-service/qf-shop-cart-service/src/main/java/com/qf/QfShopCartServiceApplication.java
+++ qf-shop-service/qf-shop-cart-service/src/main/java/com/qf/QfShopCartServiceApplication.java
@@ -1,9 +1,13 @@
 package com.qf;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@MapperScan("com.qf.mapper")
 public class QfShopCartServiceApplication {
 
     public static void main(String[] args) {
--- qf-shop-web/qf-shop-cart/src/main/java/com/qf/QfShopCartApplication.java
+++ qf-shop-web/qf-shop-cart/src/main/java/com/qf/QfShopCartApplication.java
@@ -1,9 +1,14 @@
 package com.qf;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
+
 public class QfShopCartApplication {
 
     public static void main(String[] args) {
